ea54fb6e55eab9b83cea2789a57e55e83f1fe079
Stewart Knapman <info@stewartknapman.com>
2015-10-25 15:29:05 +1300

    add update and delete tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,15 +3,30 @@ class TasksController < ApplicationController
   def index
     @new_task = Task.new
     @tasks = Task.all
+    @incomplete = @tasks.where complete: false
   end
 
   def create
+    Task.create task_params
+    redirect_to :root
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+    redirect_to :root
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to :root
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :complete)
   end
 
 end
